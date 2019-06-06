243605919eceec25dc0f6f6a39760771a1fce90f
Liu Shuai <957419821@qq.com>
2019-06-06 12:14:56 +0800

    Update logger.py
    更新：使用了一个自定义的FileHandler类。
    功能说明：
    根据指定的文件名，创建一个符号链接，指向当前时间的日志文件。
    举例来说，假设当前时间是2019年6月6日10时36分23秒，在创建日志类的时候指定文件名是test_log.log，则程序将创建一个符号链接test_log.log，其指向当前的日志文件test_log.log.2019060610；当程序不中断，且时间来到2019年6月6日11时0分0秒的时候，程序将创建新的日志文件test_log.log.2019060611，并将符号链接test_log.log指向这个新文件，而日志也将在新文件中得到保存。

--- logger.py
+++ logger.py
@@ -1,32 +1,86 @@
-#! encode=utf-8
+#! encoding=utf-8
+
 import logging
+import time
+import os
+
+class MyFileHandler(logging.FileHandler):
+    def __init__(self, filename, mode='a', encoding=None, delay=0):
+        self.__timeformat__ = "%Y%m%d%H"
+        self.rawFilename = filename
+        self.latestTime = time.strftime(self.__timeformat__, time.localtime())
+        self.filename = "%s.%s" % ( filename, self.latestTime )
+        self.mode = mode
+        self.encoding = encoding
+        self.delay = delay
+        self.baseFilename = os.path.abspath(self.filename)
+        self.__createFile()
+        super(MyFileHandler, self).__init__(self.rawFilename, mode, encoding, delay)
+    def close(self):
+        logging.FileHandler.close(self)
+    def _open(self):
+        return logging.FileHandler._open(self)
+    def emit(self, record):
+        currentTime = time.strftime(self.__timeformat__, time.localtime())
+        if currentTime != self.latestTime:
+            self.latestTime = currentTime
+            self.filename = "%s.%s" % (self.rawFilename, self.latestTime)
+            self.__createFile()
+        logging.FileHandler.emit(self, record)
+    def __createFile(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as fout:
+                pass
+        self.baseFilename = os.path.abspath(self.filename)
+        if not os.path.exists(self.rawFilename):
+            os.symlink(self.baseFilename, self.rawFilename)
+        elif not os.path.islink(self.rawFilename):
+            os.remove(self.rawFilename)
+            os.symlink(self.baseFilename, self.rawFilename)
+        elif os.path.realpath(self.rawFilename).split(".")[0] != self.filename.split(".")[0]:
+            os.remove(self.rawFilename)
+            os.symlink(self.baseFilename, self.rawFilename)
+            self.stream = super(MyFileHandler, self)._open()
 
 class Logger():
     def __init__(self, logname):
         '''
-           指定保存日志的文件路径
+           指定保存日志的文件
            将日志存入到该文件中
         '''
         # 创建一个logger
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
-
         # 创建一个handler，用于写入日志文件
-        fh = logging.FileHandler(logname)
+        #fh = logging.FileHandler(logname)
+        fh = MyFileHandler(logname)
         fh.setLevel(logging.DEBUG)
-
-        # 定义handler的输出格式，这是日志文件的行的格式
+        # 定义handler的输出格式
         string = '[%(levelname)s %(asctime)s %(process)d %(pathname)s:%(lineno)d %(funcName)s] %(message)s'
         formatter = logging.Formatter(string)
         fh.setFormatter(formatter)
-
         # 给logger添加handler
         self.logger.addHandler(fh)
-
     def createLogger(self):
         return self.logger
 
+def getLogger(logname):
+    logger = Logger(logname)
+    return logger.createLogger()
+
+def testLogger1():
+    logger.warning("ceshi")
+
+def testLogger2():
+    logger.warning("这是一个简单的测试")
+    logger.warning("这是一个简单的测试")
+
+
 if __name__ == "__main__":
     # 程序将会被记录在文件 testfile.log 中
-    logger = Logger(logname='testfile.log').createLogger()
-    logger.warning("ceshi")
+    #logger = Logger(logname='testfile.log').createLogger()
+    logger = getLogger("log/testfile.log")
+    for i in range(180):
+        testLogger1()
+        testLogger2()
+        time.sleep(1)
